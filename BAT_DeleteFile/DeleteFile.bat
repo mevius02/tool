c47254c89ef294cc63a1469c3ca0bd5c2c8bfc22
mevius02 <54784585+mevius02@users.noreply.github.com>
2026-03-24 14:55:05 +0900

    Delete BAT_DeleteFile directory

--- BAT_DeleteFile/DeleteFile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo :::::::::: 削除バッチ 開始 ::::::::::::::::::::::::::::::::::::::::
-powershell -ExecutionPolicy Bypass -File ".\DeleteScript1.ps1"
-powershell -ExecutionPolicy Bypass -File ".\DeleteScript2.ps1"
-echo :::::::::: 削除バッチ 終了 ::::::::::::::::::::::::::::::::::::::::
-pause
